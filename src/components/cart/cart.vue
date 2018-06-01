7525d6a0b6db1bce7b5db72816c4a917aac92bd6
William <menzongwei@126.com>
2018-06-02 07:49:40 +0800

    script apaces

--- src/components/cart/cart.vue
+++ src/components/cart/cart.vue
@@ -101,7 +101,7 @@ export default {
   filters: {
     currency
   },
-  //
+  // h
   created() {
     const locationData = localStorage.getItem('cartInfo');
     if (!locationData) { // nothing in cart
