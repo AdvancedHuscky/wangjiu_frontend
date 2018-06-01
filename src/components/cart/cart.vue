1daa30f4c6970ddf9bfacc46036629bd124b816e
William <menzongwei@126.com>
2018-06-02 07:50:57 +0800

    ln sock

--- src/components/cart/cart.vue
+++ src/components/cart/cart.vue
@@ -101,7 +101,7 @@ export default {
   filters: {
     currency
   },
-  // hhhsdd
+  // hhhsddddd
   created() {
     const locationData = localStorage.getItem('cartInfo');
     if (!locationData) { // nothing in cart
