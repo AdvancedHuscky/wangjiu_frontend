262dd637f22fe308fb09f6f479b8e059efb53580
William <menzongwei@126.com>
2018-06-02 07:51:32 +0800

    sadebug

--- src/components/cart/cart.vue
+++ src/components/cart/cart.vue
@@ -101,7 +101,7 @@ export default {
   filters: {
     currency
   },
-  // hhhsddddd
+  // hhhsdddddsss
   created() {
     const locationData = localStorage.getItem('cartInfo');
     if (!locationData) { // nothing in cart
