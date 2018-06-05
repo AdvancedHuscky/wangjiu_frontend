3e54fea80fcca8328c33ae107110540a55f8bd39
William <menzongwei@126.com>
2018-06-05 21:54:10 +0800

    yes enen

--- src/components/detail/shop.vue
+++ src/components/detail/shop.vue
@@ -113,7 +113,7 @@ export default {
       console.log(tempData);
       this.cartNum = tempData.num;
     }
-    // hehe haha
+    // hehe haha yaya ""
     Axios.get(`/detail?id=${this.id}`).then(({ data }) => {
       this.goodArr = data.result.list[0].list
       console.log(111);
